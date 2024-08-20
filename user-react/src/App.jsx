2258ca9f640502ef8346521519ef9aa8ccf5b197
Mpdanish <danishmaliyekkal786@gmail.com>
2024-08-20 19:59:07 +0530

    i

--- user-react/src/App.jsx
+++ user-react/src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import Header from './Header/Header'
-import Footer from './Footer/footer'
-import OnlinePurchase from './components/onlinePurchase/OnlinePurchase'
-import Login from './Login/Login'
-import Register from './Register/Register'
 import Layoutroutes from './routes/Layoutroutes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
