614a12344387a6c1fd81550b11a4bf60c41f35a6
Muhammed-Nishan-K <nishank855@gmail.com>
2024-08-20 12:59:10 +0530

    user fontend started

--- user-react/src/App.jsx
+++ user-react/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header/Header'
+import Footer from './Footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Header/>
+      <Footer/>
     </>
   )
 }
