e090057634946a715b251daa52633fc9b4ed61c7
Mpdanish <danishmaliyekkal786@gmail.com>
2024-08-20 19:46:34 +0530

    register and login page added

---
+++ user-react/src/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+
+import OnlinePurchase from "../components/onlinePurchase/OnlinePurchase";
+import Footer from "../Footer/footer";
+import Header from "../Header/Header";
+
+export default function HomePage(){
+    return(
+        <>
+        <Header />
+        <OnlinePurchase />
+        <Footer />
+        </>
+        
+    )
+}
