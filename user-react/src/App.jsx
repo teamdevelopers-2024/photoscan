aabf6f050007c454e947b044ce5389ad7441ad42
Mpdanish <danishmaliyekkal786@gmail.com>
2024-08-20 15:46:04 +0530

    ss

--- user-react/src/App.jsx
+++ user-react/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Header/Header'
 import Footer from './Footer/footer'
+import Carousel from './components/Carousel/Carousel'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Carousel />
       <Footer/>
     </>
   )
