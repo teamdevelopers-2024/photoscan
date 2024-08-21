7f3bff617d502db23c750b1f9bb0a6cdae7dd70d
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-08-21 15:55:28 +0530

    sample

--- user-react/src/main.jsx
+++ user-react/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ThemeProvider } from '@material-tailwind/react';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
     <App />
-    </ThemeProvider>
   </StrictMode>,
 )
