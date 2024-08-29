6ff601b5b9e238ea26518515abb182c4316bc7a7
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-08-29 21:45:17 +0530

    sample

---
+++ user-frontend/src/pages/Profile/Components/ChangePassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChangePassword() {
+  return (
+    <div className='p-6 w-[82%] h-[60vh] left-[18%] relative bg-white rounded-lg shadow-md'>
+        <h1 className='font-extrabold w-auto text-xl'>Currently Unavailable</h1>
+    </div>
+  )
+}
+
+export default ChangePassword
