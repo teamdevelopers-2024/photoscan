46af92cbe3a1f891e447aba591663090586c7bed
shaheen5115 <ahammedshahmkd5115.com>
2024-11-06 00:48:21 +0530

    about us sett

--- user-frontend/src/pages/AboutUsPage/Aboutus.jsx
+++ user-frontend/src/pages/AboutUsPage/Aboutus.jsx
@@ -5,6 +5,9 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import AboutUsSection from '../../components/AboutUsSection/AboutUsSection';
 import AboutUsSection1 from '../../components/AboutUsSection1/AboutUsSection1';
+import img3 from "../../assets/aboutus/image3.png"
+import img4 from "../../assets/aboutus/image4.png"
+import img5 from "../../assets/aboutus/image5.png"
 
 
 
@@ -21,25 +24,25 @@ export default function Aboutus() {
     const newsData = [
         {
           id: 1,
-          date: "12 February 2023",
-          title: "What Curling Irons Are The Best Ones",
-          image: "https://i.postimg.cc/2y6wbZCm/news1.jpg",
+          date: "",
+          title: "momentos collection",
+          image: img3,
           link: "https://www.vogue.com/article/best-curling-irons",
           delay:0
         },
         {
           id: 2,
-          date: "17 February 2023",
-          title: "The Health Benefits Of Sunglasses",
-          image: "https://i.postimg.cc/9MXPK7RT/news2.jpg",
+          date: "",
+          title: "Customised Frame Collections",
+          image: img4,
           link: "https://www.rivieraopticare.com/blog/314864-the-health-benefits-of-wearing-sunglasses_2/",
           delay:400
         },
         {
           id: 3,
-          date: "26 February 2023",
-          title: "Eternity Bands Do Last Forever",
-          image: "https://i.postimg.cc/x1KKdRLM/news3.jpg",
+          date: "",
+          title: "Customised Gift Collections",
+          image: img5,
           link: "https://www.briangavindiamonds.com/news/eternity-bands-symbolize-love-that-lasts-forever/",
           delay:800
         },
@@ -51,8 +54,8 @@ export default function Aboutus() {
     <AboutUsSection/>
     <section id="news" className="py-12 bg-gray-50">
       <div data-aos="fade-up" className="text-center mb-12">
-        <p className="text-pink-600 font-semibold text-sm uppercase">LATEST NEWS</p>
-        <h2 className="text-3xl font-bold text-gray-900">Fashion New Trends</h2>
+        <p className="text-pink-600 font-semibold text-sm uppercase"></p>
+        <h2 className="text-3xl font-bold text-gray-900">Our Collections</h2>
       </div>
       <div data-aos="fade-up" className="container mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 px-4">
         {newsData.map((news) => (
@@ -71,7 +74,7 @@ export default function Aboutus() {
                 <p className="text-sm">{news.date}</p>
               </div>
               <h4 className="text-lg font-semibold text-gray-800 mb-4">{news.title}</h4>
-              <a href={news.link} target="_blank" rel="noopener noreferrer" className="text-pink-600 hover:underline text-sm">read more</a>
+              <a href={news.link} target="_blank" rel="noopener noreferrer" className="text-pink-600 hover:underline text-sm">Go to collection</a>
             </div>
           </div>
         ))}
