65a841c9dc30af3f35edc2a23fc5f71fd87e8b09
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-11-09 23:28:10 +0530

    custom dropdowwn path issue

--- admin-frontend/src/components/mainComponents/mainDashbord/MainDashboard.jsx
+++ admin-frontend/src/components/mainComponents/mainDashbord/MainDashboard.jsx
@@ -4,7 +4,7 @@ import html2canvas from "html2canvas";
 import "./MainDashboard.css";
 import Chart from "chart.js/auto";
 import { Line, Pie } from "react-chartjs-2";
-import CustomDropdown from "./customDropDown.jsx";
+import CustomDropdown from "./CustomDropDown.jsx";
 import api from "../../../services/api.js";
 
 function MainDashbord() {
