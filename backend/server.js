9b20ba7c01aca6dc65971d7de278970a0dfbe4d6
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-11-10 02:46:12 +0530

    htaccess added admin

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: ['http://localhost:4200', 'http://localhost:5000', 'http://localhost:3000', 'http://localhost:3001','http://192.168.31.121:3000', 'https://photoscan.co.in','https://admin.photoscan.co.in'],
+    origin: ['http://localhost:4200', 'http://localhost:5001', 'http://localhost:3000', 'http://localhost:3001','http://192.168.31.121:3000', 'https://photoscan.co.in','https://admin.photoscan.co.in'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -30,8 +30,8 @@ app.use(session({
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27018/sessions' }), // Change port here
     cookie: {
-        secure: false, // true in production, false in development
-        sameSite: "none",
+        secure: false, // Should be true in production if using HTTPS
+        sameSite: "strict",
         maxAge: 1 * 60 * 60 * 1000 // 1 hour
     }
 }));
