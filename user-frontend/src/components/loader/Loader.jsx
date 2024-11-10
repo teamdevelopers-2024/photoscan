2abdc37eae18dd4dd517a6ed48c56e5f94156ac5
Muhammed Sinan Mp <sinanmhd5817@gmail.com>
2024-11-10 10:41:17 +0530

    order completed

--- user-frontend/src/components/loader/Loader.jsx
+++ user-frontend/src/components/loader/Loader.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import gifSpinn from '../../assets/spinner gifs/3dgifmaker90747.gif'
+import React, { useEffect } from 'react';
+import gifSpinn from '../../assets/spinner gifs/3dgifmaker90747.gif';
 
 function Loader({ product }) {
+  useEffect(() => {
+    // Disable scroll on mount
+    document.body.classList.add('no-scroll');
+    // Enable scroll on unmount
+    return () => document.body.classList.remove('no-scroll');
+  }, []);
+
   return (
-<div className={`flex z-10 justify-center absolute  items-center h-screen ${!product ?'bg-black w-full':'w-[60%]'} bg-opacity-70 backdrop-blur-sm`}>
-  <span className="sr-only">Loading...</span>
-  {/* Replace with your loading GIF */}
-  <img src={gifSpinn} alt="Loading animation" className="h-20 w-20 object-contain" />
-</div>
-  )
+    <div className={`flex z-10 justify-center items-center fixed top-0 left-0 right-0 bottom-0 ${!product ? 'bg-black' : 'w-[60%]'} bg-opacity-70 backdrop-blur-sm`}>
+      <span className="sr-only">Loading...</span>
+      <img src={gifSpinn} alt="Loading animation" className="h-20 w-20 object-contain" />
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
